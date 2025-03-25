80320ebbff5c3ff490b3a6b20f6fee68145f3aa0
jai-max <kadyanaryan744@gmail.com>
2025-03-25 10:09:24 +0530

    search params

--- Backend/controllers/post.controller.js
+++ Backend/controllers/post.controller.js
@@ -3,19 +3,71 @@ import Post from "../models/post.model.js"
 import User from '../models/user.model.js'
 
 export const getPosts = async (req, res) => {
+   const page = parseInt(req.query.page) || 1
+   const limit = parseInt(req.query.limit) || 2
 
-const page = parseInt(req.query.page) || 1
-const limit = parseInt(req.query.limit) || 2
+   const query = {}
 
-   const posts = await Post.find()
-   .populate("user", "username")
-   .limit(limit)
-   .skip((page - 1) * limit)
+   console.log(req.query)
+
+   const car = req.query.car
+   const author = req.query.author
+   const searchQuery = req.query.search
+   const sortQuery = req.query.sort
+   const featured = req.query.featured
+
+   if (car) {
+      query.category = car
+   }
+
+   if (searchQuery) {
+      query.title = { $regex: searchQuery, $options: 'i' }
+   }
+
+   if (author) {
+      const user = await User.findOne({ username: author }).select("_id");
+
+      if (!user) {
+         return res.status(404).json("User not found!")
+      }
+
+      query.user = user._id
+   }
+
+   let sortObj = {createdAt: -1}
+
+   if (sortQuery) {
+      switch (sortQuery) {
+         case 'newest':
+            sortObj = {createdAt: -1}
+            break;
+         case 'oldest':
+            sortObj = {createdAt: 1}
+            break;
+         case 'popular':
+            sortObj = {visit: -1}
+            break;
+         case 'trending':
+            sortObj = {visit: -1}
+            query.createdAt = {
+               $gte: new Date(new Date().getTime() - 7 * 24 * 60 * 60 * 1000),
+            }
+            break;
+         default:
+            break;
+      }
+   }
+
+   const posts = await Post.find(query)
+      .populate("user", "username")
+      .sort(sortObj)
+      .limit(limit)
+      .skip((page - 1) * limit)
 
    const totalPosts = await Post.countDocuments()
    const hasMore = page * limit < totalPosts
 
-   res.status(200).json({posts, hasMore})
+   res.status(200).json({ posts, hasMore })
 }
 
 export const getPost = async (req, res) => {
@@ -28,16 +80,16 @@ export const createPost = async (req, res) => {
 
    console.log(req.headers)
 
-   if(!clerkUserId){
+   if (!clerkUserId) {
       return res.status(401).json("Not authenticated!")
    }
-   
-   const user = await User.findOne({clerkUserId})
-   if(!user){
+
+   const user = await User.findOne({ clerkUserId })
+   if (!user) {
       return res.status(404).json("User not found!")
    }
 
-   const newPost = new Post({user: user._id, ...req.body })
+   const newPost = new Post({ user: user._id, ...req.body })
 
    const post = await newPost.save()
    res.status(200).json(post)
@@ -51,56 +103,56 @@ export const deletePost = async (req, res) => {
 
    const role = req.auth.sessionClaims?.metadata?.role || "user"
 
-   if(role === 'admin'){
+   if (role === 'admin') {
       await Post.findByIdAndDelete(req.params.id)
       return res.status(200).json("Post has been deleted!")
    }
 
    const user = await user.findOne({ clerkUserId })
-   
-   const deletedPost = await Post.findOneAndDelete({_id: req.params.id, user:user._id})
 
-   if(!deletedPost){
-   return res.status(403).json("You can delete only your posts!")
+   const deletedPost = await Post.findOneAndDelete({ _id: req.params.id, user: user._id })
+
+   if (!deletedPost) {
+      return res.status(403).json("You can delete only your posts!")
    }
-       res.status(200).json("Post has been deleted!")
+   res.status(200).json("Post has been deleted!")
+}
+
+export const featurePost = async (req, res) => {
+   const clerkUserId = req.auth.userId
+   const postId = req.body.postId
+   if (!clerkUserId) {
+      return res.status(401).json("Not authenticated!")
    }
-
-   export const featurePost = async(req, res) =>{
-      const clerkUserId = req.auth.userId
-      const postId = req.body.postId
-      if(!clerkUserId){
-         return res.status(401).json("Not authenticated!")
-      }
-      const role = req.auth.sessionClaims?.metadata?.role || "user"
-      if(role !== 'admin'){
-         return res.status(403).json("You cannot feature posts!")
-      }
-
-     const post = await Post.findById(postId)
-
-     if(!post) {
-     return res.status(404).json("Post not found!")
-     }
-
-      const isFeatured = post.isFeatured
-
-      const updatedPost = await Post.findByIdAndUpdate(
-         postId, {
-         isFeatured: !isFeatured,
-      },
-   {new: true}
-)
-      res.status(200).json(updatedPost)
+   const role = req.auth.sessionClaims?.metadata?.role || "user"
+   if (role !== 'admin') {
+      return res.status(403).json("You cannot feature posts!")
    }
 
-   const imagekit = new ImageKit({
-      urlEndpoint: 'process.env.IMAGEKIT_URL_ENDPOINT',
-      publicKey: 'process.env.IMAGEKIT_PUBLIC_KEY',
-      privateKey:' process.env.IMAGEKIT_PRIVATE_KEY',
-   })
+   const post = await Post.findById(postId)
 
-   export const uploadAuth = async(req, res)=>{
-      const result = imagekit.getAuthenticationParameters()
-      res.send(result)
-   } 
+   if (!post) {
+      return res.status(404).json("Post not found!")
+   }
+
+   const isFeatured = post.isFeatured
+
+   const updatedPost = await Post.findByIdAndUpdate(
+      postId, {
+      isFeatured: !isFeatured,
+   },
+      { new: true }
+   )
+   res.status(200).json(updatedPost)
+}
+
+const imagekit = new ImageKit({
+   urlEndpoint: 'process.env.IMAGEKIT_URL_ENDPOINT',
+   publicKey: 'process.env.IMAGEKIT_PUBLIC_KEY',
+   privateKey: ' process.env.IMAGEKIT_PRIVATE_KEY',
+})
+
+export const uploadAuth = async (req, res) => {
+   const result = imagekit.getAuthenticationParameters()
+   res.send(result)
+} 
